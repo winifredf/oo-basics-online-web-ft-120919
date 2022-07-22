01ab10a9383b25eb7ab8351c448e9b1c106e6992
winifredf <winifred.frierson@gmail.com>
2022-07-22 15:28:51 -0400

    defined Book class with properties w setter/getter

--- lib/book.rb
+++ lib/book.rb
@@ -1,39 +1,112 @@
-class Book
+# class Book
   
-  def initialize(title)
-    @title = title
+#   def initialize(title)
+#     @title = title
+#     end
+
+
+#   def title
+#     @title
+#   end
+  
+#   def author=(author)
+#     @author = author
+#   end
+  
+#   def author
+#     @author
+#   end
+  
+#   def page_count=(num)
+#     @page_count = num
+#   end
+  
+#   def page_count
+#     @page_count
+#   end
+  
+#   def genre=(genre)
+#     @genre = genre
+#   end
+  
+#   def genre
+#     @genre
+#   end
+  
+#   def turn_page
+#     puts  "Flipping the page...wow, you read fast!"
+#   end
+# end
+
+# class Book
+
+#   attr_accessor :title, :author_name, :page_count, :genre
+
+#   def initialize(title)
+#     @title = title
+    
+#   end
+  
+#   def author_name
+#     @author_name
+#   end
+
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+# end
+
+
+class Book
+    
+    def initialize (title)
+        @title = title
     end
 
+    def title
+        @title
+    end
 
-  def title
-    @title
-  end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
-  
-  def turn_page
-    puts  "Flipping the page...wow, you read fast!"
-  end
+    def author=(author)
+        @author = author
+    end
+    
+    def author
+        @author
+    end
+
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def page_count=(num)
+        @page_count = num
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
