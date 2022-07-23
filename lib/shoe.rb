66b52e05eaa4deb2c1c896e29e5c26f794641a6c
winifredf <winifred.frierson@gmail.com>
2022-07-23 11:35:26 -0400

    added attr_accessor and attr_reader properties

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -53,50 +53,16 @@
 
 class Shoe
 
+    attr_accessor :color, :size, :condition, :material
+    attr_reader :brand
+
     def initialize(brand)
     @brand = brand
     end
 
     def brand
         @brand
-    end
-
-    def color=(color)
-        @color = color
-    end
-
-    def color
-        @color
-    end
-
-    def size=(size)
-        @size = size
-    end
-
-    def size
-        @size
-    end
-
-    def material=(material)
-        @material = material
-    end
-
-    def material
-        @material
-    end
-
-    def condition=(condition)
-        @condition = condition
-    end
-
-    def condition
-        @condition
-    end
-
-    def cobble(cobble)
-        @cobble = cobble
-    end
-    
+    end   
 end
 
 
